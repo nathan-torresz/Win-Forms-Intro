7338b2e7b97d666a4fe8b325ee8db2d623d09940
nathan-torresz <nathan_henrique2015@outlook.com>
2021-05-03 02:26:09 -0300

    Tela de Login Final

--- Win_Forms_1/Win_Forms_1/Form2.Designer.cs
+++ Win_Forms_1/Win_Forms_1/Form2.Designer.cs
@@ -49,6 +49,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
             this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form2";
             this.ResumeLayout(false);
 
